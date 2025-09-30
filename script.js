b0aead45ff65adce33c6395213142cd3b277c099
EDUeran1-rgb <EDUeran1@utb.karlskoga.se>
2025-09-30 14:54:26 +0200

    hm2

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ document.addEventListener('DOMContentLoaded', function() {
     const ruta=document.getElementById("ruta");
 
     btn.addEventListener('click',function(){
-    ruta.classList.add('synlig');
-    ruta.classList.remove('osynlig');
-    console.log('ruta synlig');
-    btn.classList.remove('synlig');
-    btn.classList.add('osynlig');
-    console.log('knapp osynlig');
-    });
+        ruta.classList.add('synlig');
+        ruta.classList.remove('osynlig');
+        console.log('ruta synlig');
+        btn.classList.remove('synlig');
+        btn.classList.add('osynlig');
+        console.log('knapp osynlig');
+    }); 
+    ruta.addEventListener('click',function(){
+        btn.classList.add('synlig');
+        btn.classList.remove('osynlig');
+        console.log('ruta osynlig');
+        ruta.classList.remove('synlig');
+        ruta.classList.add('osynlig');
+        console.log('knapp synlig');
+    }); 
 
 });
